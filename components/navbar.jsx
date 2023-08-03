891dd3a7b66ea937bcfc0a56afa81e4c7dd7effe
PhillipMagos <phillipmagos@gmail.com>
2023-08-02 21:15:53 -0500

    test3

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,8 +3,12 @@ import Link from 'next/link'
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-            <Link className="text-white font-bold" href={"/"}>Blog.</Link>
-            <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
+            <Link className="text-white font-bold" href={"/"}>
+                Blog.
+            </Link>
+            <Link className="bg-white p-2" href={"/addTopic"}>
+                Add Topic
+            </Link>
         </nav>
     )
 }
