9aec8a97bb87f2a8153275ebde32ae87ab9f9cae
PhillipMagos <phillipmagos@gmail.com>
2023-08-02 21:49:55 -0500

    test9

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import Navbar from "components/Navbar.jsx";
+import Nav from "../components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="max-w-3xl mx-auto p-4">
-        <Navbar />
+        <Nav />
         <div className="mt-8">{children}</div>
         </div>
         </body>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link'
 
-export default function Navbar() {
+export default function Nav() {
     return (
         <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
             <Link className="text-white font-bold" href={"/"}>
