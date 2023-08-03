860a6775485e17d9e3da29c4df12a3765b8092f0
PhillipMagos <phillipmagos@gmail.com>
2023-08-02 23:20:11 -0500

    13

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,27 +1,27 @@
-"use client"
+"use client";
 
-import { HiOutlineTrash } from "react-icons/hi"
+import { HiOutlineTrash } from "react-icons/hi";
 import { useRouter } from "next/navigation";
 
-export default function RemoveBtn({ id}) {
-        const router = useRouter();
-        const removeTopic = async () => {
-        const confirmed = confirm('Are you sure?');
+export default function RemoveBtn({ id }) {
+  const router = useRouter();
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure?");
 
-        if (confirmed) {
-            const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
-                method: "DELETE",
-            });
+    if (confirmed) {
+      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
 
-            if (res.ok) {
-                router.refresh();
-            }
-        }
+      if (res.ok) {
+        router.refresh();
+      }
     }
+  };
 
-    return (
-        <button onClick={removeTopic} className="text-red-400">
-            <HiOutlineTrash size={24} />
-        </button>
-    );
+  return (
+    <button onClick={removeTopic} className="text-red-400">
+      <HiOutlineTrash size={24} />
+    </button>
+  );
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,15 +1,14 @@
-"use client"
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-            <Link className="text-white font-bold" href={"/"}>
-                Blog.
-            </Link>
-            <Link className="bg-white p-2" href={"/addTopic"}>
-                Add Topic
-            </Link>
-        </nav>
-    )
+  return (
+    <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+      <Link className="text-white font-bold" href={"/"}>
+        GTCoding.
+      </Link>
+      <Link className="bg-white p-2" href={"/addTopic"}>
+        Add Topic
+      </Link>
+    </nav>
+  );
 }
